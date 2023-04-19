37aa9225a29387e122093f9674f6247b33dd6a9c
Kimin Kim <kim.ki@northeastern.edu>
2023-04-19 19:15:30 +0000

    Fix viewing drug info

--- pages/Student1_edit_list/jsobjects/JSObject1/JSObject1.js
+++ pages/Student1_edit_list/jsobjects/JSObject1/JSObject1.js
@@ -2,12 +2,12 @@ export default {
 	myVar1: [],
 	myVar2: {},
 	myFun1 () {
-		get_drug_info.run();
-		if (view_drug.selectedOptionLabel != "")
-			showModal('Modal1');
 	},
 	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
+		if (view_drug.selectedOptionLabel != ""){
+			await get_drug_info.run();
+			storeValue("drug_data", get_drug_info.data);
+			showModal('Modal1');
+		}
 	}
 }
