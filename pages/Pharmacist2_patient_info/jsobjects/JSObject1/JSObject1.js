df0f6262a522ec63a64d9ab7662052afda5d7aee
Kimin Kim <kim.ki@northeastern.edu>
2023-04-19 18:58:46 +0000

    Additional features and fixes

--- pages/Pharmacist2_patient_info/jsobjects/JSObject1/JSObject1.js
+++ pages/Pharmacist2_patient_info/jsobjects/JSObject1/JSObject1.js
@@ -1,6 +1,6 @@
 export default {
 	async myFun2 () {
-		storeValue("current_drug", Text2.text)
+		storeValue("current_drug", encodeURIComponent(List1.triggeredItem.MName))
 		await get_indication.run()
 		showModal('Modal1')
 	}
---
+++ pages/Student1_edit_list/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		get_drug_info.run();
+		if (view_drug.selectedOptionLabel != "")
+			showModal('Modal1');
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
